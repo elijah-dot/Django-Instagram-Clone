4c48ea3ae3a8efe7dbc57a8bdae768d80162264d
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-06 05:31:36 +0300

    notification:changed models and made make migrations

--- notification/urls.py
+++ notification/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from notification.views import ShowNotification, DeleteNotification
 
 urlpatterns = [
-    
+    path('', ShowNotification, name='show-notification'),
+    path('<noti_id>/delete', DeleteNotification, name='delete-notification'),
 
-    
+
 ]
