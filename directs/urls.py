99bdee2ab1ee72bfb68cebb92292da0f2232bb45
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-06 05:29:52 +0300

    directs:changed models and made make migrations

--- directs/urls.py
+++ directs/urls.py
@@ -1,8 +1,10 @@
+from directs.views import inbox, Directs, SendDirect, UserSearch, NewConversation
 from django.urls import path
-from . import views
 
 urlpatterns = [
-   
-
-    
+    path('', inbox, name="message"),
+    path('direct/<username>', Directs, name="directs"),
+    path('send/', SendDirect, name="send-directs"),
+    path('search/', UserSearch, name="search-users"),
+    path('new/<username>', NewConversation, name="conversation"),
 ]
