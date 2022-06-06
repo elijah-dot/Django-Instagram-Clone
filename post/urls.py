312eb8eb27b811a1a654e2f690bc1e8a10d2b161
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-06 05:21:49 +0300

    postapp:changed models and made make migrations

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
+from post.views import index, NewPost, PostDetail, Tags, like, favourite
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('', index, name='index'),
+    path('newpost', NewPost, name='newpost'),
+    path('<uuid:post_id>', PostDetail, name='post-details'),
+    path('tag/<slug:tag_slug>', Tags, name='tags'),
+    path('<uuid:post_id>/like', like, name='like'),
+    path('<uuid:post_id>/favourite', favourite, name='favourite'),
 
-    
 ]
+
