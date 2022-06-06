04c8ba0517505b6d53d347ff44f0b8f1a2c20778
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-06 11:19:53 +0300

    index.html:fixed a bug

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%_$h)vn%@o&x-xdatn13s!i#iggig6@audcunt+-5+%xyvv4i0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['magnicant-instagram.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
