1975dd7b67a28e546794e4b4673e163a557da728
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-07 05:48:37 +0300

    readme:started on the readme file

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%_$h)vn%@o&x-xdatn13s!i#iggig6@audcunt+-5+%xyvv4i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =True
 
 ALLOWED_HOSTS = ['magnicant-instagram.herokuapp.com','127.0.0.1']
 
